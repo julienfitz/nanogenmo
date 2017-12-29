d66a31faa2638ddfe407f08a32b32ead7ccdd8d1
Lee Baillie <lbaillie00@gmail.com>
2017-12-29 17:00:08 -0500

    Trying some new things, still a ton of compiler errors though

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ mod common;
 use common::tokio_core::reactor;
 
 use egg_mode::search::{self, ResultType};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn any_alphabetic(word: &str) -> bool {
+    word.chars().all(|c| c.is_alphabetic()) // && !word.contains("#")
+}
 
 fn main() {
     // start with an initial search term
@@ -17,7 +23,11 @@ fn main() {
     let mut word_count = 0;
 
     // start with an empty vector that we will fill up with tweets
-    let mut final_tweets = vec![];
+    let mut final_tweets = "".to_string();
+    let mut tweet_collection = vec![];
+
+    // create an empty file
+    let mut file = File::create("nanogenmo.txt");
 
     // this stuff is from hyper/egg_mode for HTTP requests to the Twitter API
     let mut core = reactor::Core::new().unwrap();
@@ -25,7 +35,7 @@ fn main() {
     let handle = core.handle();
 
     // until we get to the desired word count, keep looping
-    while word_count < 100 {
+    while word_count < 10000 {
         // print the current search_term so we can be sure it's being reassigned on each loop
         println!("INITIAL SEARCH TERM: {:?}", search_term);
 
@@ -33,18 +43,41 @@ fn main() {
         // and are in English
         let search_results = &core.run(search::search(search_term.to_string() + " -filter:replies -filter:retweets -filter:media -filter:links lang:en")
                                      .result_type(ResultType::Recent)
-                                     .count(10)
+                                     .count(100)
                                      .call(&config.token, &handle)).unwrap();
 
-        // find the first tweet that's not a reply or truncated and get its text
-        // (the search filter doesn't appear to always remove all replies)
-        let tweet = &search_results.statuses.iter().find(|&toot| !&toot.text.contains("@") && !&toot.text.contains("t.co") && !final_tweets.contains(&toot.text)).unwrap().text;
+        for status in &search_results.statuses {
+            tweet_collection.push(status.clone());
+        }
 
+        let mut tweet;
+
+        // first, pull from the tweet_collection so we don't have to make another API call
+        if tweet_collection.iter().any(|&toot|
+            !&toot.text.contains("@") &&
+            !&toot.text.contains("t.co") &&
+            !final_tweets.contains(&toot.text)) {
+            tweet = tweet_collection.iter().find(|&toot| !&toot.text.contains("@") && !&toot.text.contains("t.co") && !final_tweets.contains(&toot.text)).unwrap().text;
+        } else {
+            // find the first tweet that's not a reply or truncated and get its text
+            // (the search filter doesn't appear to always remove all replies)
+            tweet = search_results.statuses.iter().find(|&toot| !&toot.text.contains("@") && !&toot.text.contains("t.co") && !final_tweets.contains(&toot.text)).unwrap().text;
+        }
+
+        // push a space onto final_tweets so they don't just run together
+        final_tweets.push_str("\n");
         // push a clone of the tweet onto final_tweets
-        final_tweets.push(tweet.clone());
+        final_tweets.push_str(&tweet.clone());
 
         // split the tweet by whitespace and clone so you can isolate the word count and last word
-        let last_word = tweet.split_whitespace().last();
+        let mut last_word = tweet.split_whitespace().nth(0);
+        for i in 0..tweet.split_whitespace().count() {
+            if !any_alphabetic(last_word.unwrap()) {
+                last_word = tweet.split_whitespace().nth(i+1);
+                break;
+            }
+        }
+
         let count = tweet.split_whitespace().count();
 
         // clone the count so we can add it to the total word count
@@ -54,8 +87,8 @@ fn main() {
         search_term = last_word.unwrap();
     }
 
+    file.unwrap().write_all(final_tweets.as_bytes());
+
     // print out all the tweets
-    for i in &final_tweets {
-        println!("{:?}", i);
-    }
+    println!("{:?}", final_tweets);
 }
